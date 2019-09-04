728dee91dc56f50dd6447d65ba3d17e1cb4c79b6
Afrânio <afranio.costa@bancointer.com.br>
2019-09-04 13:27:38 -0300

    teste btn movendo

--- src/components/ButtonTransition.js
+++ src/components/ButtonTransition.js
@@ -3,3 +3,33 @@
  * Quando o botao for clicado, este deverá fazer uma transicao de 30% da metade da largura da tela
  * Ex.: se a tela for 480px de largura, entao o botao tera um transition do ponto 240px até 30% desse valor
  */
+import React, {useState,useEffect} from 'react'
+import '../css/cor-btn.css';
+
+function ButtonTransition(){
+
+const[moveButton, setMoveButton] = useState(0);
+
+document.addEventListener('resize', function(){
+    const windowWidth = window.innerWidth
+    console.log('a largura é' windowWidth)
+})
+
+useEffect(()=>{
+  
+})
+
+return(
+    <>
+      <a
+        className='btnPosition'
+        href='qualquer/coisa'
+        onClick={evt => { this.setMoveButton() }}
+      >
+        Click
+      </a>
+    </>
+)
+}
+
+export default ButtonTransition
